9f816b7a5b51d9daf110329565518e56c7d33ba3
psdevorder <65866800+psdevorder@users.noreply.github.com>
2020-08-06 13:20:55 +0200

    first commit

---
+++ scripts/forecast.js
@@ -0,0 +1,33 @@
+const key = 'sUD2MbTJZuipF9200AcaWMoeWEhNk382';
+
+//get weather info
+const getWeather = async (id) => {
+    const base ='http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+//get city info
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+// getCity('zagreb').then(data => {
+//     return getWeather(data.Key)
+// }).then(data => {
+//     console.log(data);
+// })
+// .catch(err => console.log(err));
+
